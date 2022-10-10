95252aca6949e05961c601d994648694699df006
Aleksandra Stoilkovic <aaaleksandraaaa@gmail.com>
2022-10-10 21:57:13 +0200

    Uploading SignupTests test4

--- src/tests/SignupTests.java
+++ src/tests/SignupTests.java
@@ -3,13 +3,13 @@ package tests;
 import org.testng.annotations.Test;
 
 public class SignupTests extends BasicTest{
-//	@Test(priority = 1)
+	@Test(priority = 1)
 	public void visitsTheSignupPage() {
 		navPage.getSignUpButton().click();
 		softAssert.assertEquals(driver.getCurrentUrl(), baseUrl + "/signup", "You are not on signup page");
 		softAssert.assertAll();
 	}
-//	@Test(priority = 2)
+	@Test(priority = 2)
 	public void checksInputTypes() {
 		navPage.getSignUpButton().click();
 		softAssert.assertEquals(signupPage.getEmailField().getAttribute("type"), 
@@ -33,8 +33,24 @@ public class SignupTests extends BasicTest{
 		signupPage.getConfirmPasswordField().sendKeys("12345");
 		signupPage.getSignMeUpButton().click();
 		messagePopUpPage.waitVerifyYourAccountDialogToBeVisible();
-		softAssert.assertTrue(messagePopUpPage.getVerifyYourAccountText().getText().contains("E-mail already exists"));
+		softAssert.assertTrue(messagePopUpPage.getVerifyYourAccountText().getText().contains("E-mail already exists"), 
+				"The dialog is not dispayed");
 		softAssert.assertEquals(driver.getCurrentUrl(), baseUrl + "/signup", "You are not on signup page");
 		softAssert.assertAll();
+	}
+	@Test(priority = 4)
+	public void signup() {
+		navPage.getSignUpButton().click();
+		signupPage.getNameField().sendKeys("Aleksandra Stoilkovic");
+		signupPage.getEmailField().sendKeys("aleksandra.stoilkovic@itbootcamp.rs");
+		signupPage.getPasswordField().sendKeys("12345");
+		signupPage.getConfirmPasswordField().sendKeys("12345");
+		signupPage.getSignMeUpButton().click();
+		driver.get(baseUrl + "/home");
+		softAssert.assertTrue(messagePopUpPage.getVerifyYourAccountText().getText().contains("IMPORTANT: Verify your account"), 
+				"The dialog is not dispayed");
+		softAssert.assertAll();
+		messagePopUpPage.getCloseButton().click();
+		navPage.getLogOutButton().click();
 	}
 }
