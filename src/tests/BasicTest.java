dfcb31d7dce1b39747ba4998201f0464f13f3274
Aleksandra Stoilkovic <aaaleksandraaaa@gmail.com>
2022-10-10 19:33:41 +0200

    Uploading Basic Test

---
+++ src/tests/BasicTest.java
@@ -0,0 +1,62 @@
+package tests;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.asserts.SoftAssert;
+
+import pages.CitiesPage;
+import pages.LoginPage;
+import pages.MessagePopUpPage;
+import pages.NavPage;
+import pages.SignupPage;
+
+public class BasicTest {
+	protected WebDriver driver;
+	protected String baseUrl = "https://vue-demo.daniel-avellaneda.com";
+	protected CitiesPage citiesPage;
+	protected LoginPage loginPage;
+	protected MessagePopUpPage messagePopUpPage;
+	protected NavPage navPage;
+	protected SignupPage signupPage;
+	protected WebDriverWait wait;
+	protected SoftAssert softAssert;
+
+	@BeforeClass
+	public void beforeClass() {
+		System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(30));
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+		citiesPage = new CitiesPage(driver, wait);
+		loginPage = new LoginPage(driver);
+		messagePopUpPage = new MessagePopUpPage(driver, wait);
+		navPage = new NavPage(driver);
+		signupPage = new SignupPage(driver);
+		softAssert = new SoftAssert();
+	}
+
+	@BeforeMethod
+	public void beforeMethod() {
+		driver.get(baseUrl);
+	}
+
+	@AfterMethod
+	public void afterMethod() {
+
+	}
+
+	@AfterClass
+	public void afterClass() {
+		driver.quit();
+	}
+
+}
