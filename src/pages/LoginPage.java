a64ebf3a5f43917246352572c5feac054b702e2a
Aleksandra Stoilkovic <aaaleksandraaaa@gmail.com>
2022-10-10 16:43:23 +0200

    Uploading Login Page

---
+++ src/pages/LoginPage.java
@@ -0,0 +1,27 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class LoginPage {
+	
+	private WebDriver driver;
+	
+
+	public LoginPage(WebDriver driver) {
+		super();
+		this.driver = driver;
+	}
+
+	public WebElement getEmailField() {
+		return driver.findElement(By.id("email"));
+	}
+	public WebElement getUsernameField() {
+		return driver.findElement(By.name("password"));
+	}
+	public WebElement getLoginButton() {
+		return driver.findElement(By.xpath("//span[text()='Login']"));
+	}
+
+}
