c723b17e45f6b21773badb93a3c6a75f20197bed
Aleksandra Stoilkovic <aaaleksandraaaa@gmail.com>
2022-10-10 19:55:16 +0200

    Uploading LoginTests test2

--- src/tests/LoginTests.java
+++ src/tests/LoginTests.java
@@ -3,12 +3,23 @@ package tests;
 import org.testng.annotations.Test;
 
 public class LoginTests extends BasicTest {
-	@Test(priority = 1)
+//	@Test(priority = 1)
 	public void visitsTheLoginPage() {
 		navPage.getLanguageButton().click();
 		navPage.getENButton().click();
 		navPage.getLogInButton().click();
 		softAssert.assertEquals(driver.getCurrentUrl(), baseUrl + "/login", "You are not on log in page");
+		softAssert.assertAll();
+	}
+	@Test(priority = 2)
+	public void checksInputTypes() {
+		navPage.getLogInButton().click();
+		softAssert.assertEquals(loginPage.getEmailField().getAttribute("type"), 
+				"email", 
+				"Type of email field shoul be email");
+		softAssert.assertEquals(loginPage.getUsernameField().getAttribute("type"), 
+				"password", 
+				"Type of username field shoul be password");
 		softAssert.assertAll();
 	}
 }
