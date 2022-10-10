6d18116284e225ea1b2f86e664f9bb4b875aff26
Aleksandra Stoilkovic <aaaleksandraaaa@gmail.com>
2022-10-10 20:19:36 +0200

    Correction of LoginPage

--- src/pages/LoginPage.java
+++ src/pages/LoginPage.java
@@ -1,19 +1,23 @@
 package pages;
 
+import java.time.Duration;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class LoginPage {
 	
 	private WebDriver driver;
-	
+	private WebDriverWait wait;
 
-	public LoginPage(WebDriver driver) {
+	public LoginPage(WebDriver driver, WebDriverWait wait) {
 		super();
 		this.driver = driver;
+		this.wait = wait;
 	}
-
 	public WebElement getEmailField() {
 		return driver.findElement(By.id("email"));
 	}
@@ -22,6 +26,10 @@ public class LoginPage {
 	}
 	public WebElement getLoginButton() {
 		return driver.findElement(By.xpath("//span[text()='Login']"));
+	}
+	public void waitForHomePageToBeVisible() {
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+		wait.until(ExpectedConditions.urlContains("/home"));
 	}
 
 }
