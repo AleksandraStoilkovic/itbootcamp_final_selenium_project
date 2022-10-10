2d075d88f79969a404519b1a1b205d2a1d161958
Aleksandra Stoilkovic <aaaleksandraaaa@gmail.com>
2022-10-10 20:07:59 +0200

    Uploading LoginTests test3

--- src/tests/LoginTests.java
+++ src/tests/LoginTests.java
@@ -3,7 +3,7 @@ package tests;
 import org.testng.annotations.Test;
 
 public class LoginTests extends BasicTest {
-//	@Test(priority = 1)
+	@Test(priority = 1)
 	public void visitsTheLoginPage() {
 		navPage.getLanguageButton().click();
 		navPage.getENButton().click();
@@ -17,9 +17,21 @@ public class LoginTests extends BasicTest {
 		softAssert.assertEquals(loginPage.getEmailField().getAttribute("type"), 
 				"email", 
 				"Type of email field shoul be email");
-		softAssert.assertEquals(loginPage.getUsernameField().getAttribute("type"), 
+		softAssert.assertEquals(loginPage.getPasswordField().getAttribute("type"), 
 				"password", 
-				"Type of username field shoul be password");
+				"Type of password field shoul be password");
 		softAssert.assertAll();
 	}
+	@Test(priority = 3)
+	public void displaysErrorsWhenUserDoesNotExist() {
+		navPage.getLogInButton().click();
+		loginPage.getEmailField().sendKeys("non-existing-user@gmal.com");
+		loginPage.getPasswordField().sendKeys("password123");
+		loginPage.getLoginButton().click();
+		messagePopUpPage.waitForPopUpCreateBeVisible();
+		softAssert.assertTrue(messagePopUpPage.getElementsThatContainText().getText().contains("User does not exists"));
+		softAssert.assertEquals(driver.getCurrentUrl(), baseUrl + "/login", "You are not on log in page");
+		softAssert.assertAll();
+	}
+	
 }
