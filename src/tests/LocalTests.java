8c6195612b1bc17d668fb18d7cbee1cdc3c73d55
Aleksandra Stoilkovic <aaaleksandraaaa@gmail.com>
2022-10-11 19:52:46 +0200

    Uploading LocalTests test 3

--- src/tests/LocalTests.java
+++ src/tests/LocalTests.java
@@ -2,19 +2,27 @@ package tests;
 
 import org.testng.annotations.Test;
 
-public class LocalTests extends BasicTest{
+public class LocalTests extends BasicTest {
 	@Test(priority = 1)
 	public void setLocaleToES() {
 		navPage.getLanguageButton().click();
 		navPage.getESButton().click();
-		softAssert.assertEquals(navPage.getHeaderText().getText(), "P�gina de aterrizaje", "Language is not set to ES");
+		softAssert.assertEquals(navPage.getHeaderText().getText(), "Página de aterrizaje", "Language is not set to ES");
 		softAssert.assertAll();
 	}
+
 	@Test(priority = 2)
 	public void setLocaleToEN() {
 		navPage.getLanguageButton().click();
 		navPage.getENButton().click();
 		softAssert.assertEquals(navPage.getHeaderText().getText(), "Landing", "Language is not set to EN");
 		softAssert.assertAll();
-}
+	}
+	@Test(priority = 3)
+	public void setLocaleToCN() {
+		navPage.getLanguageButton().click();
+		navPage.getCNButton().click();
+		softAssert.assertEquals(navPage.getHeaderText().getText(), "首页", "Language is not set to CN");
+		softAssert.assertAll();
+	}
 }
