ec30e5e34284d98dda390650c2570442ac691403
Aleksandra Stoilkovic <aaaleksandraaaa@gmail.com>
2022-10-11 19:00:19 +0200

    Uploading AdminCitiesTests test 4

--- src/tests/AdminCitiesTests.java
+++ src/tests/AdminCitiesTests.java
@@ -1,5 +1,7 @@
 package tests;
 
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.Test;
 
 public class AdminCitiesTests extends BasicTest {
@@ -35,6 +37,29 @@ public class AdminCitiesTests extends BasicTest {
 		messagePopUpPage.waitForPopUpCreateBeVisible();
 		softAssert.assertTrue(messagePopUpPage.getElementsThatContainTextWhenWeSaveNewCity().getText().contains("Saved successfully"), 
 				"Message Pop Up should contain text Saved sucessfully");
-
 	}
+	@Test(priority = 4)
+	public void editCity() throws InterruptedException {
+		navPage.getAdminButton().click();
+		navPage.getCitiesLink().click();
+		citiesPage.getNewItemButton().click();
+		citiesPage.waitForDialogForEditAndCreateBeVisible();
+		citiesPage.getNameInput().sendKeys("Aleksandra Stoilkovic’s city");
+		citiesPage.getSaveButton().click();
+		citiesPage.waitForSpecificNumberOfRowsToBeVisible(1);
+		citiesPage.getEditButtonFromRow(1).click();
+		Actions a = new Actions(driver);
+		a.keyDown(Keys.CONTROL);
+		a.keyDown("A");
+		a.keyDown(Keys.DELETE);
+		a.keyUp(Keys.CONTROL);
+		a.keyUp("A");
+		a.keyUp(Keys.DELETE);
+		citiesPage.getNameInput().sendKeys("Aleksandra Stoilkovic’s city Edited");
+		citiesPage.getSaveButton().click();
+		messagePopUpPage.waitForPopUpCreateBeVisible();
+		softAssert.assertTrue(messagePopUpPage.getElementsThatContainTextWhenWeSaveNewCity().getText().contains("Saved successfully"), 
+				"Message Pop Up should contain text Saved sucessfully");
+	}
+	
 }
